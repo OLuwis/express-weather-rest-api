95d5b66e0a44916ab613a48c05a6df45b002b207
Luis Miguel <oluismrs@gmail.com>
2023-07-29 01:34:02 -0300

    test: refactored tests

--- __tests__/users/users.test.ts
+++ __tests__/users/users.test.ts
@@ -1,11 +1,22 @@
 import { app } from "@src/app.js";
-import AppDataSource from "@src/db.js";
+import { AuthService } from "@auth/auth.service.js";
+import { userRepository } from "@src/db.js";
+import { AppDataSource } from "@src/db.js";
+import bcrypt from "bcrypt";
 import request from "supertest";
 
 describe("POST /user/signup", () => {
-    beforeAll(async () => await AppDataSource.initialize());
+    beforeAll(async () => {
+        await AppDataSource.initialize();
+        const authService = new AuthService();
+        await authService.signup("repeatedUser", "password");
+    });
     
-    afterAll(async () => await AppDataSource.destroy());
+    afterAll(async () => {
+        await userRepository.delete({ username: "repeatedUser" });
+        await userRepository.delete({ username: "testUser" });
+        await AppDataSource.destroy();
+    });
     
     describe("GIVEN a user and password", () => {
         const bodyMock = { username: "testUser", password: "testPassword" };
@@ -15,6 +26,7 @@ describe("POST /user/signup", () => {
             expect(response.body.message).toBe(`${bodyMock.username} Created!`);
         });
     });
+
     describe("GIVEN only a username/password", () => {
         const bodyMock = { username: "testUser", password: "" };
         it("should THEN send 400 status code and 'Information Is Missing!' message", async () => {
@@ -23,6 +35,7 @@ describe("POST /user/signup", () => {
             expect(response.body.message).toBe("Information Is Missing!");
         });
     });
+    
     describe("GIVEN a small password", () => {
         const bodyMock = { username: "testUser", password: "test" };
         it("should THEN send 400 status code and 'Passwords Must Have At Least 8 Characters!' message", async () => {
@@ -31,6 +44,7 @@ describe("POST /user/signup", () => {
             expect(response.body.message).toBe("Passwords Must Have At Least 8 Characters!");
         });
     });
+
     describe("GIVEN a username already registered", () => {
         const bodyMock = { username: "repeatedUser", password: "testPassword" };
         it("should THEN send 409 status code and 'Username Already Registered!' message", async () => {
@@ -40,3 +54,4 @@ describe("POST /user/signup", () => {
         });
     });
 });
+
