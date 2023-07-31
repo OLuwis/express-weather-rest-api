b6868e66db51af56aa39d2210184909df60c512c
Luis Miguel <oluismrs@gmail.com>
2023-07-31 17:36:03 -0300

    test: add more locations tests

--- __tests__/api/auth/auth.test.ts
+++ __tests__/api/auth/auth.test.ts
@@ -1,46 +1,39 @@
 import { app } from "@src/app.js";
 import { AuthService } from "@auth/auth.service.js";
-import { userRepository } from "@src/db.js";
+import { userRepo } from "@src/db.js";
 import { AppDataSource } from "@src/db.js";
 import request from "supertest";
 import jsonwebtoken from "jsonwebtoken";
-import { before } from "node:test";
 
 describe("POST /api/auth/signup", () => {
     beforeAll(async () => {
         await AppDataSource.initialize();
-        const authService = new AuthService();
-        await authService.signup({ username: "repeatedUser", password: "testPassword"});
     });
     
     afterAll(async () => {
-        await userRepository.delete({ username: "repeatedUser" });
-        await userRepository.delete({ username: "testUser" });
+        await userRepo.delete({ username: "username" });
         await AppDataSource.destroy();
     });
     
     describe("GIVEN a user and password", () => {
         it("should THEN send 201 status code and user data in body", async () => {
-            const bodyMock = { username: "testUser", password: "testPassword" };
-            const response = await request(app).post("/api/auth/signup").send(bodyMock);
+            const response = await request(app).post("/api/auth/signup").send({ username: "username", password: "password" });
             expect(response.statusCode).toBe(201);
-            expect(response.body.message).toBe(`${bodyMock.username} Created!`);
+            expect(response.body.message).toBe("username Created!");
         });
     });
 
     describe("GIVEN only a username/password", () => {
-        it("should THEN send 400 status code and 'Information Is Missing!' message", async () => {
-            const bodyMock = { username: "testUser", password: "" };
-            const response = await request(app).post("/api/auth/signup").send(bodyMock);
+        it("should THEN send 400 status code and 'Information Is Missing!'", async () => {
+            const response = await request(app).post("/api/auth/signup").send({ username: "username", password: "" });
             expect(response.statusCode).toBe(400);
             expect(response.body.message).toBe("Information Is Missing!");
         });
     });
     
     describe("GIVEN a small password", () => {
-        it("should THEN send 400 status code and 'Passwords Must Have At Least 8 Characters!' message", async () => {
-            const bodyMock = { username: "testUser", password: "test" };
-            const response = await request(app).post("/api/auth/signup").send(bodyMock);
+        it("should THEN send 400 status code and 'Passwords Must Have At Least 8 Characters!'", async () => {
+            const response = await request(app).post("/api/auth/signup").send({ username: "username", password: "word" });
             expect(response.statusCode).toBe(400);
             expect(response.body.message).toBe("Passwords Must Have At Least 8 Characters!");
         });
@@ -48,8 +41,7 @@ describe("POST /api/auth/signup", () => {
 
     describe("GIVEN a username already registered", () => {
         it("should THEN send 409 status code and 'Username Already Registered!' message", async () => {
-            const bodyMock = { username: "repeatedUser", password: "testPassword" };
-            const response = await request(app).post("/api/auth/signup").send(bodyMock);
+            const response = await request(app).post("/api/auth/signup").send({ username: "username", password: "password" });
             expect(response.statusCode).toBe(409);
             expect(response.body.message).toBe("Username Already Registered!");
         });
@@ -60,17 +52,17 @@ describe("POST /api/auth/login", () => {
     beforeAll(async () => {
         await AppDataSource.initialize();
         const authService = new AuthService();
-        await authService.signup({ username: "testUser", password: "testPassword" });
+        await authService.signup({ username: "username", password: "password" });
     });
     
     afterAll(async () => {
-        await userRepository.delete({ username: "testUser" });
+        await userRepo.delete({ username: "username" });
         await AppDataSource.destroy();
     });
 
     describe("GIVEN only a username/password", () => {
         it("should THEN send 400 status code and 'Information Is Missing!' message", async () => {
-            const bodyMock = { username: "testUser", password: "" };
+            const bodyMock = { username: "username", password: "" };
             const response = await request(app).post("/api/auth/signup").send(bodyMock);
             expect(response.statusCode).toBe(400);
             expect(response.body.message).toBe("Information Is Missing!");
@@ -79,7 +71,7 @@ describe("POST /api/auth/login", () => {
     
     describe("GIVEN a small password", () => {
         it("should THEN send 400 status code and 'Passwords Must Have At Least 8 Characters!' message", async () => {
-            const bodyMock = { username: "testUser", password: "test" };
+            const bodyMock = { username: "username", password: "word" };
             const response = await request(app).post("/api/auth/signup").send(bodyMock);
             expect(response.statusCode).toBe(400);
             expect(response.body.message).toBe("Passwords Must Have At Least 8 Characters!");
@@ -88,7 +80,7 @@ describe("POST /api/auth/login", () => {
     
     describe("GIVEN a correct username and password", () => {
         it("should THEN send 200 status code and token on body", async () => {
-            const bodyMock = { username: "testUser", password: "testPassword" };
+            const bodyMock = { username: "username", password: "password" };
             const response = await request(app).post("/api/auth/login").send(bodyMock);
             expect(response.statusCode).toBe(200);
             const jwtPayload = <{ username: string }>jsonwebtoken.decode(response.body.token);
@@ -97,7 +89,7 @@ describe("POST /api/auth/login", () => {
     });
 
     describe("GIVEN a wrong password", () => {
-        const bodyMock = { username: "testUser", password: "wrongPassword" };
+        const bodyMock = { username: "username", password: "wrongPassword" };
         it("should THEN send 401 status code and 'Wrong Password!' message", async() => {
             const response = await request(app).post("/api/auth/login").send(bodyMock);
             expect(response.statusCode).toBe(401);
@@ -106,7 +98,7 @@ describe("POST /api/auth/login", () => {
     });
 
     describe("GIVEN a wrong user", () => {
-        const bodyMock = { username: "wrongUser", password: "testPassword" };
+        const bodyMock = { username: "username2", password: "password" };
         it("should THEN send 401 status code and 'Username Not Found!' message", async() => {
             const response = await request(app).post("/api/auth/login").send(bodyMock);
             expect(response.statusCode).toBe(401);
--- __tests__/api/location/location.test.ts
+++ __tests__/api/location/location.test.ts
@@ -1,69 +1,180 @@
-import { app } from "@src/app.js";
-import { AppDataSource } from "@src/db.js";
 import request from "supertest";
+import { app } from "@src/app.js";
+import jsonwebtoken from "jsonwebtoken";
+import { AuthService } from "@auth/auth.service.js";
+import { AppDataSource, locationRepo, userRepo } from "@src/db.js";
 
 describe("GET /api/locations/search?q=", () => {
-    beforeAll(() => {
-
-    });
-
-    afterAll(() => {
-
-    });
-
     describe("GIVEN a city name", () => {
-        it("should THEN return a status code 200 and an array of objects containing city names", async () => {
-            const response = await request(app).get("/api/locations/search?q=Salvador");
-            expect(response.statusCode).toBe(200);
-            expect(typeof response.body).toBe("Array");
-            expect(response.body[0]).toHaveProperty("name", "Salvador");
+        it("should THEN return a status code 200 and an array of objects with multiple keys", async () => {
+            const { statusCode, body } = await request(app).get("/api/locations/search?q=Salvador");
+            expect(statusCode).toBe(200);
+            for (let i = 0; i < body - 1; i++) {
+                expect(body[i].data).toHaveProperty("city");
+                expect(body[i].data).toHaveProperty("state");
+                expect(body[i].data).toHaveProperty("country");
+                expect(body[i].data).toHaveProperty("lat");
+                expect(body[i].data).toHaveProperty("lon");
+                expect(body[i].ref).toHaveProperty("weather");
+                expect(body[i].ref).toHaveProperty("forecast");
+                expect(body[i].ref).toHaveProperty("save");
+            };
+        });
+    });
+
+    describe("GIVEN no search terms", () => {
+        it("should THEN return a status code 400 and 'No Search Terms Found!'", async () => {
+            const response = await request(app).get("/api/locations/search?q=");
+            expect(response.statusCode).toBe(400);
+            expect(response.body.message).toBe("No Search Terms Found!");
         });
     });
 });
 
 describe("POST /api/locations", () => {
-    beforeAll(() => {
-
+    const bodyObj = {
+        city: "Salvador",
+        state: "Bahia",
+        country: "BR",
+        lat: -12.98,
+        lon: -38.48
+    };
+    
+    beforeAll(async () => {
+        await AppDataSource.initialize();
+        const authService = new AuthService();
+        await authService.signup({ username: "username", password: "password" });
     });
 
-    afterAll(() => {
-
+    afterAll(async () => {
+        const userTest = await userRepo.findOneBy({ username: "username" });
+        await locationRepo.delete({ user_id: userTest?.user_id });
+        await userRepo.delete({ username: "username" });
+        await AppDataSource.destroy();
     });
 
-    describe("GIVEN a city data", () => {
-        it("should THEN return a status code 201 and a 'Location Saved!' message", async () => {
-            const response = await request(app).post("/api/locations").send({
-                name: "Salvador",
-                lat: -12.9822499,
-                lon: -38.4812772,
-                country: "BR",
-                state: "Bahia"
-            });
-            expect(response.statusCode).toBe(201);
-            expect(response.body.message).toBe("Location Saved!");
+    describe("GIVEN a location data from an authenticated user", () => {
+        it("should THEN return a status code 201 and 'Location Saved!'", async () => {
+            const auth = await request(app).post("/api/auth/login").send({ username: "username", password: "password" });
+            const { statusCode, body } = await request(app).post("/api/locations").send(bodyObj).set("Authorization", `Bearer ${auth.body.token}`);
+            expect(statusCode).toBe(201);
+            expect(body.message).toBe("Location Saved!");
+        });
+    });
+    
+    describe("GIVEN a unauthorized user request", () => {
+        it("should THEN return a status code 401 and 'Not Authorized!'", async () => {
+            const { statusCode, body } = await request(app).post("/api/locations").send(bodyObj);
+            expect(statusCode).toBe(401);
+            expect(body.message).toBe("Not Authorized!");
         });
     });
 });
 
-describe("GET /api/locations/:id", () => {
-    describe("GIVEN an user id", () => {
-        it("should THEN if authenticated return a status code 200 and an array of saved locations", async () => {
-            
+describe("GET /api/locations", () => {
+    const bodyObj = {
+        city: "Salvador",
+        state: "Bahia",
+        country: "BR",
+        lat: -12.98,
+        lon: -38.48
+    };
+    
+    beforeAll(async () => {
+        await AppDataSource.initialize();
+        const authService = new AuthService();
+        await authService.signup({ username: "username", password: "password" });
+    });
+
+    afterAll(async () => {
+        const userTest = await userRepo.findOneBy({ username: "username" });
+        await locationRepo.delete({ user_id: userTest?.user_id });
+        await userRepo.delete({ username: "username" });
+        await AppDataSource.destroy();
+    });
+    
+    describe("GIVEN an request from authenticated user", () => {
+        it("should THEN return a status code 200 and an array of objects with multiple keys", async () => {
+            const auth = await request(app).post("/api/auth/login").send({ username: "username", password: "password" });
+            await request(app).post("/api/locations").send(bodyObj).set("Authorization", `Bearer ${auth.body.token}`);
+            const { statusCode, body } = await request(app).get("/api/locations").set("Authorization", `Bearer ${auth.body.token}`);
+            expect(statusCode).toBe(200);
+            for (let i = 0; i < body.length; i++) {
+                expect(body[i].data).toHaveProperty("location_id");
+                expect(body[i].data).toHaveProperty("city");
+                expect(body[i].data).toHaveProperty("state");
+                expect(body[i].data).toHaveProperty("country");
+                expect(body[i].data).toHaveProperty("lat");
+                expect(body[i].data).toHaveProperty("lon");
+                expect(body[i].ref).toHaveProperty("weather");
+                expect(body[i].ref).toHaveProperty("forecast");
+                expect(body[i].ref).toHaveProperty("delete");
+            };
+        });    
+    });
+});
+
+describe("DELETE /api/locations/:id", () => {
+    const bodyObj = {
+        city: "Salvador",
+        state: "Bahia",
+        country: "BR",
+        lat: -12.98,
+        lon: -38.48
+    };
+    
+    beforeAll(async () => {
+        await AppDataSource.initialize();
+        const authService = new AuthService();
+        await authService.signup({ username: "username", password: "password" });
+    });
+
+    afterAll(async () => {
+        await userRepo.delete({ username: "username" });
+        await AppDataSource.destroy();
+    });
+
+    describe("GIVEN a location id", () => {
+        it("should THEN return a status code 204 and 'Location Deleted!'", async () => {
+            const auth = await request(app).post("/api/auth/login").send({ username: "username", password: "password" });
+            const { user_id } = <{ user_id: number }>jsonwebtoken.decode(auth.body.token);
+            await request(app).post("/api/locations").send(bodyObj).set("Authorization", `Bearer ${auth.body.token}`);
+            const testLocation = await locationRepo.findOneBy({ user_id: user_id });
+            const { statusCode, body } = await request(app).delete(`/api/locations/${testLocation?.location_id}`).set("Authorization", `Bearer ${auth.body.token}`);
+            expect(statusCode).toBe(404);
+            expect(body.message).toBe("Location Deleted!");
         });
     });
 });
 
 describe("GET /api/locations/weather?lat=&lon=", () => {
-    describe("", () => {
-        it("", async () => {
+    beforeAll(async () => {
+        await AppDataSource.initialize();
+    });
+
+    afterAll(async () => {
+        await AppDataSource.destroy();
+    });
+    
+    describe("GIVEN a latitude and longitude", () => {
+        it("should THEN return a status code 200 and an object with weather data", async () => {
+            const response = await request(app).get("/api/locations/weather?lat=-12.98&-38.48");
 
         });
     });
 });
 
-describe("GET /api/locations/weather?lat=&lon=", () => {
-    describe("", () => {
-        it("", async () => {
+describe("GET /api/locations/forecast?lat=&lon=", () => {
+    beforeAll(async () => {
+        await AppDataSource.initialize();
+    });
+
+    afterAll(async () => {
+        await AppDataSource.destroy();
+    });
+    
+    describe("GIVEN a latitude and longitude", () => {
+        it("should THEN return a status code 200 and an array of object with weather data", async () => {
 
         });
     });
